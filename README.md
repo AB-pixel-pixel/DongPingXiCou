0919874f850ad25ba79868a506c39ca87b64d286
还没睡 <89346035+AB-pixel-pixel@users.noreply.github.com>
2022-04-19 19:24:57 +0800

    Add files via upload
    web test

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # -
 东拼西凑组作品
+
+----服务器搭建----
+目前服务器仅供测试
+
+## 主要参考教程：
+
+### 安装flask
+教程
+https://flask.net.cn/installation.html
+
+### 如何让代码运行
+进入DongPingXiCou文件夹
+在终端依次输入
+export FLASK_APP=upload_image.py
+export FLASK_ENV=development
+
+下面这行代码会让你的操作系统监听所有公开的IP，大概的范围是局域网
+（连接同一个wifi的设备，一个校区内的校园网我也不知道算不算是同一个wifi，我在宿舍用两台电脑连校园网，他们确实可以互相通讯）
+flask run --host=0.0.0.0 
+下面这行 仅限于本机内通信
+flask run 
+
+
+### 服务器的搭建主要参考这个
+https://flask.net.cn/patterns/fileuploads.html
+
+# 目前以实现功能为主要目的，没有怎么考虑安全性或者前端设计
---
+++ upload_image.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/home/ab/Project/save' # 图片储存路径
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg'} # 输入格式仅限于这三种
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+    
+@app.route('/',methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            app.config["path"]=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        try:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
+        except FileNotFoundError :
+            return redirect(url_for('result'))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/result',methods=['GET','POST'])
+def result():
+    return f'{app.config["path"]}'
