7affa82cd1782a04e9a3fcc4515413d719424097
还没睡 <89346035+AB-pixel-pixel@users.noreply.github.com>
2022-04-19 21:26:14 +0800

    add fake network
    for test only

--- upload_image.py
+++ upload_image.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import random
 
 UPLOAD_FOLDER = '/home/ab/Project/save' # 图片储存路径
 ALLOWED_EXTENSIONS = {'png','jpg','jpeg'} # 输入格式仅限于这三种
@@ -8,6 +9,10 @@ ALLOWED_EXTENSIONS = {'png','jpg','jpeg'} # 输入格式仅限于这三种
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+# 假神经网络
+def network_infer():
+    random.randint(0,1)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -29,8 +34,13 @@ def upload_file():
         try:
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
         except FileNotFoundError :
-            return redirect(url_for('result'))
-
+            return redirect(url_for('error'))
+        finally:
+            result = network_infer()
+            if result :
+                return redirect(url_for('real'))
+            else:
+                return redirect(url_for('fake'))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -41,6 +51,14 @@ def upload_file():
     </form>
     '''
 
-@app.route('/result',methods=['GET','POST'])
-def result():
+@app.route('/error',methods=['GET','POST'])
+def error():
     return f'{app.config["path"]}'
+
+@app.route('/real',method=['GET','POST'])
+def real():
+    return f'图片为真'
+
+@app.route('/fake',method=['GET','POST'])
+def fake():
+    return f'图片为假'
