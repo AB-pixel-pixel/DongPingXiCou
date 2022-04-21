76af8b153714adcabf8b05524738ea947c2fd9c3
还没睡 <89346035+AB-pixel-pixel@users.noreply.github.com>
2022-04-21 13:05:36 +0800

    Add files via upload

---
+++ Dong/flask/openvino_deploy/network.py
@@ -0,0 +1,110 @@
+from openvino.runtime import Core, PartialShape 
+from openvino.offline_transformations import serialize
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+"""可以独立测试"""
+
+# 此处修改文件路径
+model_path="model/deepfake_detection_model.xml"
+extract_face_model_path="model/haarcascade_frontalface_alt2.xml"
+image_path="../../save/test.jpg" 
+
+test = 0
+show_face = 1
+reshape_model_batch_size = False
+enable_caching = False
+
+def extract_face(image_path="../../save/test.jpg",\
+    extract_face_model_path="model/haarcascade_frontalface_alt2.xml"):
+    '''提取图像中的一张人脸，输出的数据可以直接放入神经网络'''
+    image = cv2.imread(filename=image_path)
+    cascade = cv2.CascadeClassifier(extract_face_model_path)
+    rects = cascade.detectMultiScale(image, \
+        scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:, 2:] += rects[:, :2]
+    for x1,y1,x2,y2 in rects:
+        # 调整人脸截取的大小。横向为x,纵向为y
+        # TODO 就是可以优化这个矩形，例如，将矩形摆正或者之类的
+        image_roi = image[y1-15 :y2+30, x1-50 :x2]
+    #  展示出人脸
+    if show_face:
+        plt.imshow(image_roi)
+    input_image = cv2.resize(image_roi, (224, 224))
+    input_image = np.expand_dims(input_image.transpose(2, 0, 1), 0)
+    return input_image 
+
+def check_device():
+    """检查设备可用情况"""
+    if test:
+        devices = ie.available_devices
+        for device in devices:
+            device_name = ie.get_property(device_name=device, name="FULL_DEVICE_NAME")
+            print(f"{device}: {device_name}")
+
+
+def deepfake_detection_network(image_path,model_path,extract_face_model_path): 
+    # init ie
+    ie = Core()
+    model = ie.read_model(model=model_path)
+    # cache model
+    """ cache_path = Path("model/model_cache")
+    cache_path.mkdir(exist_ok=True)
+    config_dict = {"CACHE_DIR": str(cache_path)}"""
+    # compile model
+    if enable_caching:
+        compiled_model = ie.compile_model(model=model,device_name="GPU", config=config_dict)
+    else:
+        compiled_model = ie.compile_model(model=model,device_name="GPU")
+    input_layer = compiled_model.input(0)
+    output_layer = compiled_model.output(0)
+    # reshape_model_batch_size
+    if reshape_model_batch_size: 
+        new_shape = PartialShape([1, 3, 224, 224])
+        model.reshape({input_layer.any_name: new_shape})
+    # check the model infomation
+    if test: 
+        print(model.input(0).any_name)
+        print(f"input precision: {input_layer.element_type}")
+        print(f"input shape: {input_layer.shape}")
+        print(model.output(0).any_name)
+        print(f"output precision: {output_layer.element_type}")
+        print(f"output shape: {output_layer.shape}")
+
+    # inference status
+    # process image
+    while True:
+        input_data = extract_face(image_path=image_path,\
+            extract_face_model_path=extract_face_model_path)
+        # 检查输入数据的形状
+        if test:
+            print("input_data.shape:",input_data.shape)
+            # inference
+        result = list(compiled_model([input_data]).values())[0][0][0]
+        if result >= 0:
+            result = 1
+        else:
+            result = 0
+        return result 
+            
+    
+
+"""以下代码用于将模型从onnx文件转换为IR文件"""
+def convert(model_path):
+    ie = Core()
+    model = ie.read_model(model=model_path)
+    serialize(model=model, model_path="model/deepfake_detection_model.xml",\
+         weights_path="model/deepfake_detection_model.bin")
+
+def main():
+    # check_device()
+    # convert(model_path)
+    print(next(deepfake_detection_network(image_path=image_path,\
+        model_path=model_path,\
+        extract_face_model_path=extract_face_model_path)))
+    # next(network(image_path=image_path,model_path=model_path))
+
---
+++ Dong/flask/upload_image.py
@@ -0,0 +1,74 @@
+import os, sys
+import random 
+from pathlib import Path
+from flask import Flask, flash, request, redirect
+from werkzeug.utils import secure_filename
+from openvino_deploy.network import deepfake_detection_network
+
+# 文件说明：
+"""上传文件后用网络处理，然后显示纯文本消息，要再次上传就要重新发送请求（刷新网页）"""
+
+
+# 路径
+"""
+注意,
+人脸识别模型名称必须为:haarcascade_frontalface_alt2.xml
+神经网络模型为IR文件:deepfake_detection_model.xml
+"""
+import os
+from pathlib import Path
+current_file_path=Path(os.path.abspath(__file__))
+save_image_path=current_file_path.parent.parent /'save'
+extract_face_model_path = current_file_path.parent \
+    / 'openvino_deploy/model/haarcascade_frontalface_alt2.xml' # 人脸识别模型路径
+extract_face_model_path=str(extract_face_model_path) # opencv函数要求参数为str类型
+model_path = current_file_path.parent \
+    / 'openvino_deploy/model/deepfake_detection_model.xml'
+
+
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}  # 接受的文件后缀名
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = save_image_path
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+    
+@app.route('/',methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            app.config["image_path"]=(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
+            # result的取值为1，则图片为真，为0，则图片为假
+            result = deepfake_detection_network(model_path = model_path,\
+                extract_face_model_path = extract_face_model_path,\
+                image_path=app.config["image_path"])
+            work = 0
+            if os.path.exists(app.config["image_path"]):
+                os.remove(app.config["image_path"])
+            if result:
+                return "real_face"
+            else:
+                return "fake_face"
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
