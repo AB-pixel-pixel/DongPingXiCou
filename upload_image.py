5a06374e5638bad4ea7ddf7e97121c25cf72d3ab
还没睡 <89346035+AB-pixel-pixel@users.noreply.github.com>
2022-04-21 11:11:12 +0800

    test 解决路径问题

--- upload_image.py
+++ upload_image.py
@@ -10,14 +10,23 @@ from openvino_deploy.network import deepfake_detection_network
 
 
 # 路径
+"""
+注意,
+人脸识别模型名称必须为:haarcascade_frontalface_alt2.xml
+神经网络模型为IR文件:deepfake_detection_model.xml
+"""
+current_file_path=Path(os.path.abspath(__file__))
+save_image_path=current_file_path.parent.parent /'save'
+extract_face_model_path = current_file_path.parent \
+    / 'openvino_deploy/model/haarcascade_frontalface_alt2.xml' # 人脸识别模型
+model_path = current_file_path.parent \
+    / 'openvino_deploy/model/deepfake_detection_model.xml'
 
-extract_face_model_path = "openvino_deploy/model/haarcascade_frontalface_alt2.xml"
-model_path="openvino_deploy/model/deepfake_detection_model.xml"
-UPLOAD_FOLDER = '/home/ab/Desktop/uploads' # 路径
+
 ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}  # 接受的文件后缀名
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = save_image_path
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 def allowed_file(filename):
@@ -36,11 +45,20 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            app.config["path"]=(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            app.config["image_path"]=(os.path.join(app.config['UPLOAD_FOLDER'],filename))
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
-        return deepfake_detection_network(model_path = model_path,\
-            extract_face_model_path = extract_face_model_path,\
-                image_path=app.config["path"])
+            # result的取值为1，则图片为真，为0，则图片为假
+            result = deepfake_detection_network(model_path = model_path,\
+                extract_face_model_path = extract_face_model_path,\
+                image_path=app.config["image_path"])
+            work = 0
+            if os.path.exists(app.config["image_path"]):
+                os.remove(app.config["image_path"])
+                work = 1
+            if result:
+                return f"real_face{work}"
+            else:
+                return f"fake_face{work}"
 
     return '''
     <!doctype html>
